0b71f6150bada1eca65f0629900511f92addd670
Eunah Kim <Eunahkim92@gmail.com>
2020-05-15 00:50:33 -0400

    Refactor profile display and styling

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-var nameLine = document.querySelector('.typewriter');
-var descLine = document.querySelector('.typewriter2');
-var nameCursor = document.querySelector('.cursor');
-var bioCursor = document.querySelector('.bio');
+const nameLine = document.querySelector('.typewriter');
+const descLine = document.querySelector('.typewriter2');
+const nameCursor = document.querySelector('.cursor');
+const bioCursor = document.querySelector('.bio');
 
-
+const content = `
+    <span class="col-md-6 text-center">
+        <h5>Skills:</h5>
+        <p class="m-0 p-0">Adobe Photoshop</p>
+        <p class="m-0 p-0">Microsoft Office</p>
+        <p class="m-0 p-0">Fluent in English & Korean</p>
+        </span>
+    <span class="col-md-6 text-center">
+        <h5>Languages:</h5>
+        <p class="m-0 p-0">HTML</p>
+        <p class="m-0 p-0">CSS</p>
+        <p class="m-0 p-0">Javascript</p>
+    </span>
+`
 const name = 'eunah kim';
 const description = 'I graduated from the University of Central Florida in 2015. I am aspiring to be a Software Developer and currently am honing and maturing my skills to be an efficient Full Stack Web Developer. My hobbies include, photography, graphic design, and traveling.'
 
@@ -22,7 +35,7 @@ typeName = () => {
         setTimeout(function() {
             nameCursor.classList.remove('cursor');
             bioCursor.classList.add('cursorMini');
-            var city = `
+            const city = `
                 <i class="fas fa-map-marker-alt fa-2x float-left ml-2"></i><h3>orlando, fl</h3>
             `
             $('#locationLine').append(city);
@@ -44,6 +57,9 @@ typeDesc = () => {
         setTimeout(function() {
             bioCursor.classList.remove('cursorMini');
         }, typingDelay + 5000);
+        setTimeout(function() {
+            $('.add-content').append(content);
+        }, typingDelay + 2000);
     }
 }
 
